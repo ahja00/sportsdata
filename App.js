557a65d1c32a6c7b692986d5e7999b9fb1b016ed
Janika Ahonen <t2ahja00@students.oamk.fi>
2024-10-03 21:40:01 +0300

    sports data

--- App.js
+++ App.js
@@ -1,10 +1,46 @@
 import { StatusBar } from 'expo-status-bar';
+import{useEffect, useState}from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+const url= "https://www.thesportsdb.com/api/v1/json/3/searchteams.php?t=Arsenal"
+
 export default function App() {
+  const [country,setStrCountry]=useState('')
+  const[gender,setStrGender]=useState('')
+  const[colour,setStrColour1]=useState('')
+
+  useEffect(()=>{
+    (async()=>{
+     await getSportData()
+  })();
+},[]);
+
+  const getSportData=async()=>{
+    try{
+      const response=await fetch(url);
+      if(response.ok) {
+     const json=await response.json();
+     const team=json.teams[0];
+     setStrCountry(team.strCountry);
+     setStrGender(team.strGender);
+     setStrColour1(team.strColour1);
+      } else {
+        if(response.status===429){
+          console.log("Too many requests!")
+        }else{
+          console.log("can't retrieve beer information!")
+        }
+      }
+    }catch(error){
+      console.log(error)
+    }
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.header}>Get sports info</Text>
+      <Text style={styles.text}>{country}</Text>
+      <Text style={styles.text}>{gender}</Text>
+      <Text style={styles.text}>{colour}</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +49,10 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#FFC0CB',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+
 });
